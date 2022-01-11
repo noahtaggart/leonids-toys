087e980f7dcb33dbda6ec76e3130abccec95c35b
Noah Taggart <noahtag@gmail.com>
2022-01-11 13:24:52 -0600

    initial push

---
+++ leonids-toys.js
@@ -0,0 +1,23 @@
+const buzzlightyear = {
+    id: 1,
+    name: 'Buzz Lightyear',
+    mfr: "Disney",
+    price: 39.84,
+
+}
+
+const woody = {
+    id:2,
+    name: "Woody",
+    mfr: "Disney",
+    price: 54.92,
+
+}
+
+const rex = {
+    id:3,
+    name: "Rex",
+    mfr: 'Disney',
+    price: 12.99,
+    
+}
